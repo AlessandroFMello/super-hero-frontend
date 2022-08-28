8cb0612fd2d77ebb410884d1697b9d9006fe27c2
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-08-28 09:12:23 -0300

    feat: :sparkles: Adds register btns and functions to SuperHeroPage

--- src/pages/SuperHeroPage.tsx
+++ src/pages/SuperHeroPage.tsx
@@ -3,9 +3,13 @@ import { AppContextType, IHero, IUniverse } from '../@types/hero';
 import DisplayBtn from '../components/DisplayBtn';
 import Header from '../components/Header';
 import HeroCard from '../components/HeroCard';
+import RegisterRenderer from '../components/RegisterRenderer';
 import UniverseCard from '../components/UniverseCard';
 import UniverseHeader from '../components/UniverseHeader';
 import { AppContext } from '../context/AppProvider';
+import apiUpdate from '../services/apiUpdate';
+import { AiOutlineClose } from "react-icons/ai";
+import apiCreate from '../services/apiCreate';
 
 export default function SuperHeroPage() {
   const {
@@ -15,8 +19,159 @@ export default function SuperHeroPage() {
     filteredHeroes,
     showHeroes,
     filteredUniverses,
+    superHero,
+    onSelectChange,
+    onNameChange,
+    onUrlChange,
+    isDisabled,
+    newUniverse,
+    onUniverseNameChange,
+    register,
+    isDisabledUniverse,
+    message,
+    addHero,
+    addUniverse,
+    setAddHero,
+    setAddUniverse,
   } = useContext(AppContext) as AppContextType;
 
+  
+
+  async function onSubmitHero(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const heroData = {
+      name: superHero.name,
+      universeId: superHero.universe,
+      imageUrl: superHero.image,
+    }
+
+    const newHero = await apiCreate(heroData, "heroes");
+
+    if (newHero) {
+      window.location.reload();
+    }
+  }
+
+  function newHeroForm() {
+    return(
+      <>
+        <form onSubmit={ (e) => onSubmitHero(e) }>
+          <select
+            className="modal-styles"
+            onChange={ (e) => onSelectChange(e) }
+            value={ superHero.universe }
+          >
+            {
+              universes.map((universe) => (
+                <option
+                  key={universe.id}
+                  value={universe.id}
+                >
+                  {universe.universe}
+                </option>
+              ))
+            }
+          </select>
+          <input
+            className="modal-styles"
+            type="text"
+            placeholder="Digite o nome do novo herói"
+            onChange={ (e) => onNameChange(e) }
+            required
+          />
+          <input
+            className="modal-styles"
+            type="text"
+            placeholder="Insira a URL da imagem"
+            onChange={ (e) => onUrlChange(e) }
+            required
+          />
+          <button
+            className="modal-styles"
+            type='submit'
+            disabled={isDisabled}
+          >
+            Criar novo heróí
+          </button>
+        </form>
+        <AiOutlineClose
+          className="close-modal-btn"
+          onClick={ () => setAddHero(!addHero) }
+        />
+      </>
+    );
+  }
+
+  async function onSubmitUniverse(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const universeData = {
+      id: newUniverse.id,
+      universe: newUniverse.universe,
+    }
+
+    const newHero = await apiCreate(universeData, "universes");
+
+    if (newHero) {
+      window.location.reload();
+    }
+  }
+
+
+  function newUniverseForm() {
+    return (
+      <>
+        <form onSubmit={ (e) => onSubmitUniverse(e) }>
+          <input
+            className="modal-styles"
+            type="text"
+            placeholder="Digite o nome do novo universo"
+            onChange={ (e) => onUniverseNameChange(e) }
+            required
+          />
+          <button
+            className="modal-styles"
+            type='submit'
+            disabled={isDisabledUniverse}
+          >
+            Criar novo universo
+          </button>
+        </form>
+        <AiOutlineClose
+          className="close-modal-btn"
+          onClick={ () => setAddUniverse(!addUniverse) }
+        />
+      </>
+    );
+  }
+
+  function registerRender() {
+    if (register === "hero") {
+      return(
+        <section
+          className="modal-form"
+        >
+          { newHeroForm() }
+          {
+            message !== "" && <div>{message}</div>
+          }
+        </section>
+      );
+    }
+    if (register === "universe") {
+      return(
+        <section
+          className="modal-form"
+        >
+          { newUniverseForm() }
+          {
+            message !== "" && <div>{message}</div>
+          }
+        </section>
+      );
+    }
+  }
   
   return (
     <>
@@ -27,6 +182,7 @@ export default function SuperHeroPage() {
           <header className="header-wrapper">
             <DisplayBtn />
             <Header />
+            <RegisterRenderer />
           </header>
           <div className="hero-wrapper">
             {
@@ -53,6 +209,7 @@ export default function SuperHeroPage() {
           <>
           <header className="header-wrapper">
             <DisplayBtn />
+            <UniverseHeader />
             <RegisterRenderer />
           </header>
           <div className="universes-wrapper">
@@ -76,6 +233,9 @@ export default function SuperHeroPage() {
           </div>
           </>
         )
+      }
+      {
+        (addHero || addUniverse) && registerRender() 
       }
     </>
   )

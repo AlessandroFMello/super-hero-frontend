412d78f180ea92a8cf4d906e24353863986dd5fe
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-08-28 09:17:54 -0300

    feat: :sparkles: Adds edit functions to HeroCard

--- src/components/HeroCard.tsx
+++ src/components/HeroCard.tsx
@@ -1,4 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
+import { AppContextType, IHero } from '../@types/hero';
+import { AppContext } from '../context/AppProvider';
+import apiGetAll from '../services/apiGetAll';
 import { BsFillTrashFill } from 'react-icons/bs';
 import { BsPencilFill } from "react-icons/bs";
 
@@ -7,7 +10,35 @@ interface BaseLayoutProps {
 }
 
 const HeroCard: React.FunctionComponent<BaseLayoutProps> = ({ data }) => {
+  const {
+    setIsDisabled,
+    superHero,
+    setSuperHero,
+    edit,
+    setEdit,
+  } = useContext(AppContext)  as AppContextType;
   const { id, name, heroUniverse, image } = data as IHero;
+  async function getSuperHero() {
+    const hero = await apiGetAll(`heroes/${id}`)
+    setSuperHero({
+      id: id,
+      name: hero.name,
+      universe: hero.universe,
+      image: hero.image
+    });
+  }
+
+  useEffect(() => {
+    function enableBtn() {
+      if (superHero.image !== "" && superHero.name !== "") {
+        setIsDisabled(false);
+      } else {
+        setIsDisabled(true);
+      }
+    }
+    enableBtn();
+  }, [superHero, setIsDisabled])
+
   return (
     <>
           <div className="hero-card">
@@ -19,6 +50,10 @@ const HeroCard: React.FunctionComponent<BaseLayoutProps> = ({ data }) => {
               <BsPencilFill
                 type='button'
                 className="menu-btn"
+                onClick={ () => {
+                  getSuperHero()
+                  setEdit(!edit)
+                }}
               />
             </div>
             <figure className="hero-card-figure">
